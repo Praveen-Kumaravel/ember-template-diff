6b84701635529fe301177a2c043230999dbbb18d
Praveen Kumaravel <praveen.kumaravel@freshworks.com>
2023-07-10 14:09:34 +0530

    Filter and print

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const recast = require('ember-template-recast');
 const treeDiffer = require('treediffer');
+const {print: printAstNode} = require('@glimmer/syntax');
 const visitorKeys = require('./visitorKeys');
 const { readFile } = require('fs/promises')
 
 async function readFileAsString(path) {  
   return await readFile(path, 'utf8')
 } 
+
+const NODE_TYPES_TO_CONSIDER = ['Template', 'Block', 'MustacheStatement',
+    'BlockStatement', 'PartialStatement', 'ElementNode',
+    'NamedBlock', 'SimpleElement', 'Component'];
 
 class ASTNode extends treeDiffer.TreeNode {
     isEqual(otherNode) {
@@ -39,17 +44,34 @@ async function run() {
         .transactions[ tree1.orderedNodes.length - 1 ][ tree2.orderedNodes.length - 1 ];
     
     console.log(diff);
+
+    let filteredTransactions = [];
+
+    function processTransaction(type, nodeA, nodeB) {
+        if (!nodeB) {
+            if (NODE_TYPES_TO_CONSIDER.includes(nodeA.type)) {
+                filteredTransactions.push(`${type} ${printAstNode(nodeA)}`);
+            }
+
+            return;
+        }
+        if (NODE_TYPES_TO_CONSIDER.includes(nodeA.type) || NODE_TYPES_TO_CONSIDER.includes(nodeB.type)) {
+            filteredTransactions.push(`${printAstNode(nodeA)} ${type} ${printAstNode(nodeB)}`);
+        }
+    }
+
     let i, ilen;
     for ( i = 0, ilen = diff.length; i < ilen; i++ ) {
 		if ( diff[ i ][ 0 ] !== null && diff[ i ][ 1 ] !== null ) {
-			console.log( 'change', tree1.orderedNodes[ diff[ i ][ 0 ] ].node.type );
-			console.log('change' , tree2.orderedNodes[ diff[ i ][ 1 ] ].node.type );
+            processTransaction('->', tree1.orderedNodes[ diff[ i ][ 0 ] ].node, tree2.orderedNodes[ diff[ i ][ 1 ] ].node)
 		} else if ( diff[ i ][ 0 ] ) {
-			console.log('remove' , tree1.orderedNodes[ diff[ i ][ 0 ] ].node.type );
+            processTransaction('-', tree1.orderedNodes[ diff[ i ][ 0 ] ].node);
 		} else if ( diff[ i ][ 1 ] ) {
-			console.log('insert' , tree2.orderedNodes[ diff[ i ][ 1 ] ].node.type );
+            processTransaction('+', tree2.orderedNodes[ diff[ i ][ 1 ] ].node);
 		}
 	}
+
+    console.log(filteredTransactions);
 }
 
 run();
